c6d87bd4a8b291c92529fce111e0cd39bf1bddf1
kimuijin <whitekuj02@naver.com>
2023-02-03 01:38:12 +0900

    개발환경 세팅

---
+++ src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+// src/main/java/com.demogroup.demoweb/Controller/HelloWorldController.java
+
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
