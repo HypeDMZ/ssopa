9bc5190e779a8c3c9a4cea993dbe9e8d32eafe0c
“ssohye” <ssohye@icloud.com>
2023-02-04 02:38:40 +0900

    sibal

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+
+
+
+
+import Profile from './Profile';
+
+
+
+function App() {
+    const [user, setUser] = useState(null);
+    const authenticated = user != null;
+
+
+    const logout = () => setUser(null);
+
+    return (
+        <Router>
+            <header>
+                <Link to="/">
+                    <button>Home</button>
+                </Link>
+                <Link to="/profile">
+                    <button>Profile</button>
+                </Link>
+                {authenticated ? (
+                    <button onClick={logout}>Logout</button>
+                ) : (
+                    <Link to="/login">
+                        <button>Login</button>
+                    </Link>
+                )}
+            </header>
+            <hr />
+            <main>
+                <Switch>
+
+
+
+
+                </Switch>
+            </main>
+        </Router>
+    );
+}
+
+export default App;
