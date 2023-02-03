628d74b92cd059c29a3eb7bcbc2cf87195b34eef
“ssohye” <ssohye@icloud.com>
2023-02-04 02:38:04 +0900

    sibal

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,10 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
