4503de10ae21ca630ecb2dd0925cf361b72eb93c
“ssohye” <ssohye@icloud.com>
2023-02-08 21:44:03 +0900

    [API명세 수정]

---
+++ src/main/java/com/example/demo/dto/ChangeNicknameRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class ChangeNicknameRequestDto {
+    @ApiModelProperty(value="닉네임", example="조태완바보",required = true)
+    private String Nickname;
+    @ApiModelProperty(value="이메일", example="ssohye@icloud.com",required = true)
+    private String email;
+}
