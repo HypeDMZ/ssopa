751a097da933fec33e8656a8491ebd15c478d0e0
“ssohye” <ssohye@icloud.com>
2023-02-09 00:21:30 +0900

    [백엔드 ] 글 작성 기능 일부 완성

---
+++ src/main/java/com/example/demo/Service/PostService.java
@@ -0,0 +1,53 @@
+package com.example.demo.Service;
+
+import com.example.demo.config.SecurityUtil;
+import com.example.demo.dto.*;
+import com.example.demo.entity.Member;
+import com.example.demo.entity.Post;
+import com.example.demo.jwt.TokenProvider;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PostService {
+
+    private final AuthenticationManagerBuilder managerBuilder;
+    private final MemberRepository memberRepository;
+
+    private final PostRepository postRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+
+
+    @Transactional
+    public PostResponseDto newpost(String title, String content) {
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다"));
+        System.out.println("로그인 정보 : "+member.getEmail());
+        Post post = Post.builder()
+                .title(title)
+                .writer(member.getNickname())
+                .content(content)
+                .created_date(LocalDateTime.now())
+                .modified_date(LocalDateTime.now())
+                .deleteYn(Boolean.FALSE)
+                .noticeYn(Boolean.FALSE)
+                .view_cnt(0)
+                .build();
+        return PostResponseDto.of(postRepository.save(post));
+
+    }
+
+
+}
