1fcdcc71f58e03530bd9a382a904d707873cabbe
kimuijin <whitekuj02@naver.com>
2023-02-10 00:10:16 +0900

    회원가입 비동기 작업 해결

--- src/main/frontend/src/component/Signup.js
+++ src/main/frontend/src/component/Signup.js
@@ -5,39 +5,42 @@ function Signup(props){
     let [비밀번호, 비밀번호변경] = useState('');
     let [이름, 이름변경] = useState('');
     let [SignUpDataJSON, SignUpDataJSONChange] = useState({});
+    let SignUpData = new Object();
 
-
+    useEffect(()=>{
+        SignUpData.email = 이메일;
+        SignUpData.nickname = 이름;
+        SignUpData.password = 비밀번호;
+        SignUpDataJSONChange(JSON.stringify(SignUpData));
+    },[이메일, 비밀번호, 이름]);
     return (
-        <div>
-            <h4>회원가입</h4>
-            <span>닉네임<input type={"text"} onChange={ (e)=>{
-                이름변경(e.target.value);
-            }}></input></span><br/>
-            <span>이메일<input type={"text"} onChange={ (e)=>{
-                이메일변경(e.target.value);
-            }}></input></span><br/>
-            <span>비밀번호<input type={"password"} onChange={ (e)=>{
-                비밀번호변경(e.target.value);
-            }}></input></span><br/>
-            <button onClick={ ()=>{
-                let SignUpData = new Object();
-                SignUpData.email = 이메일;
-                SignUpData.nickname = 이름;
-                SignUpData.password = 비밀번호;
-
-                SignUpDataJSONChange(JSON.stringify(SignUpData));
-                console.log(SignUpDataJSON);
-
-                axios.post("http://localhost:8080/auth/signup",
-                    SignUpDataJSON,
-                    {
-                        withCredentials : true,
-                        headers : {"Content-Type": 'application/json'}
-                    })
-                    .then((response) => { console.log(response.data); })
-                    .catch((response) => { console.log('Error!') });
-            }}>회원가입</button>
-        </div>
+        <>
+            <div>
+                <h4>회원가입</h4>
+                <span>닉네임<input type={"text"} value={이름} onChange={ (e)=>{
+                    이름변경(e.target.value);
+                }}></input></span><br/>
+                <span>이메일<input type={"text"} value={이메일} onChange={ (e)=>{
+                    이메일변경(e.target.value);
+                }}></input></span><br/>
+                <span>비밀번호<input type={"password"} value={비밀번호} onChange={ (e)=>{
+                    비밀번호변경(e.target.value);
+                }}></input></span><br/>
+            </div>
+            <div>
+                <button onClick={ ()=>{
+                    console.log(SignUpDataJSON);
+                    axios.post("http://localhost:8080/auth/signup",
+                        SignUpDataJSON,
+                        {
+                            withCredentials : true,
+                            headers : {"Content-Type": 'application/json'}
+                        })
+                        .then((response) => { console.log(response.data); })
+                        .catch((response) => { console.log('Error!') });
+                }}>회원가입</button>
+            </div>
+        </>
     )
 }
 
