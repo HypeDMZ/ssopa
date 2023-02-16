9c8e2f0208bbfba0c2f33f1fbcfc79694ea5b6d7
kimuijin <whitekuj02@naver.com>
2023-02-16 21:14:22 +0900

    login

--- src/main/frontend/src/component/Login.js
+++ src/main/frontend/src/component/Login.js
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import axios, {get} from 'axios';
 import Layout from "./layout/Layout";
 import {setCookie,getCookie,removeCookie} from "../function/cookie";
+import "../css/Login.css";
+import {useMediaQuery} from "react-responsive";
 
 function Login(props){
     let [아이디, 아이디변경] = useState('');
@@ -15,6 +17,13 @@ function Login(props){
         loginData.password = 비밀번호;
         loginDataJSONChange(JSON.stringify(loginData));
     },[아이디,비밀번호]);
+
+    const Min = () =>{
+        const isMin = useMediaQuery({ minWidth: 768});
+        {/*null 자리에 MoblieLogin */}
+        return isMin ? <LoginMainPC/> : null;
+    }
+
 
     const onLogin = () => {
         console.log(loginDataJSON);
@@ -47,20 +56,31 @@ function Login(props){
             })
             .catch((response) => { console.log('Error!') });
     }
+
+    const LoginMainPC = () => {
+        return(
+            <>
+                <div className={"login-main"}/>
+                <div className={"login-dark"}/>
+                <div className={"login-title"}>행복해지는 길</div>
+                <div className={"login-option"} style={{top:"38.37%" , left:"30%"}}>email</div>
+                <div className={"login-option"} style={{top:"46.87%" , left:"28%"}}>password</div>
+                <input type={"text"} className={"login-input"} style={{left: "39.1%", top: "38.28%"}}
+                       onChange={ (e)=>{아이디변경(e.target.value);}}/>
+                <input type={"password"} className={"login-input"} style={{left: "39.1%", top: "47.55%"}}
+                       onChange={ (e)=>{비밀번호변경(e.target.value);}}/>
+                <button className={"login-button"} onClick={ ()=>{
+                    onLogin();
+                }}>로그인</button>
+                <Link to="/auth/signup" className={"login-signup-nav"}>아니 아직 회원이 아니라고?</Link>
+            </>
+        )
+    }
+
     return (
         <Layout>
-            <h4>홈</h4>
-            <input type={"text"} onChange={ (e)=>{
-                아이디변경(e.target.value);
-            }}></input><br/>
-            <input type={"password"} onChange={ (e)=>{
-                비밀번호변경(e.target.value);
-            }}></input><br/>
-            <button onClick={ ()=>{
-                onLogin();
-            }}>로그인</button>
+            <Min/>
         </Layout>
     )
 }
-
 export { Login };
