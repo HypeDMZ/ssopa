b77c213878fd153ae0f88eef3243591d73b0ac88
kimuijin <whitekuj02@naver.com>
2023-02-19 04:32:36 +0900

    채팅 일단 돼써요..

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from "react-cookie";
+import axios from "axios";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
