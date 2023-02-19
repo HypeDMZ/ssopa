38916c46d67d367b1008722bc65b15ad3de460d2
“ssohye” <ssohye@icloud.com>
2023-02-19 15:32:39 +0900

    cors

--- src/main/java/com/example/demo/Controller/MemberController.java
+++ src/main/java/com/example/demo/Controller/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@CrossOrigin(origins = "http://localhost:3000")
 @Api(tags = "MemberController : 회원 정보 요청 관련 컨트롤러")
 public class MemberController {
 
