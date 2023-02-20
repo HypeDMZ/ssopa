bfa3c70aee4c02ddd9b1b2822e3364e82127f5f7
taewan <taewan2002@naver.com>
2023-02-20 17:27:13 +0900

    [create] AuthController add exception feature

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
@@ -40,42 +41,70 @@ public class AuthController {
     @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginrequest) {
-        return httpResponseUtil.createOKHttpResponse(authService.login(loginrequest), "로그인 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.login(loginrequest), "로그인 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("로그인 실패: " + e.getMessage());
+        }
     }
 
     @Operation(summary = "토큰 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(@RequestBody TokenReqDto tokenRequestDto) {
-        return httpResponseUtil.createOKHttpResponse(authService.reissue(tokenRequestDto), "토큰 재발급 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.reissue(tokenRequestDto), "토큰 재발급 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("토큰 재발급 실패: " + e.getMessage());
+        }
     }
 
     @Operation(summary = "인증문자 보내기")
     @GetMapping("/check/sendSMS")
     public ResponseEntity<?> sendSMS(@RequestParam(value="to") String to){
-        return httpResponseUtil.createOKHttpResponse(authService.PhoneNumberCheck(to), "인증번호 전송 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.PhoneNumberCheck(to), "인증문자 보내기 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("인증문자 보내기 실패: " + e.getMessage());
+        }
     }
 
     @GetMapping("/check/verifySMS")
     @Operation(summary = "인증문자 확인")
     public ResponseEntity<?> verifySMS(@RequestParam(value="to") String to,@RequestParam(value="code") String code){
-        return httpResponseUtil.createOKHttpResponse(authService.verifySms(code,to), "인증번호 확인 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.verifySms(to,code), "인증문자 확인 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("인증문자 확인 실패: " + e.getMessage());
+        }
     }
 
     @PostMapping("/findId")
     @Operation(summary = "아이디 찾기")
     public ResponseEntity<?> findID(@RequestBody FindIdDto dto) {
-        return httpResponseUtil.createOKHttpResponse((authService.findID(dto.getName(), dto.getPhonenumber())), "아이디 찾기 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse((authService.findID(dto.getName(), dto.getPhonenumber())), "아이디 찾기 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("아이디 찾기 실패: " + e.getMessage());
+        }
     }
 
     @PostMapping("/findId/veritfySMS")
     @Operation(summary = "아이디 찾기 인증문자 확인")
     public ResponseEntity<?> findVerifyedID(@RequestBody FindVerifyedDto dto){
-        return httpResponseUtil.createOKHttpResponse(authService.findIdverifySms(dto.getCode(),dto.getPhonenumber()), "인증번호 확인 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.findIdverifySms(dto.getCode(),dto.getPhonenumber()), "아이디 찾기 인증문자 확인 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("아이디 찾기 인증문자 확인 실패: " + e.getMessage());
+        }
     }
 
     @PostMapping("/resetpassword")
     @Operation(summary = "비밀번호 재설정")
     public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordDto dto){
-        return httpResponseUtil.createOKHttpResponse(authService.resetPassword(dto.getEmail(),dto.getPassword(), dto.getPasswordConfirm()), "비밀번호 재설정 성공");
+        try {
+            return httpResponseUtil.createOKHttpResponse(authService.resetPassword(dto.getEmail(),dto.getPassword(), dto.getPasswordConfirm()), "비밀번호 재설정 성공");
+        } catch (Exception e) {
+            return httpResponseUtil.createInternalServerErrorHttpResponse("비밀번호 재설정 실패: " + e.getMessage());
+        }
     }
 }
