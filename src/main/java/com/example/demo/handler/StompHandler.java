505a2f22e54747a242e1877746d6d4dd3da5be1f
“ssohye” <ssohye@icloud.com>
2023-02-23 21:31:38 +0900

    fix

--- src/main/java/com/example/demo/handler/StompHandler.java
+++ src/main/java/com/example/demo/handler/StompHandler.java
@@ -40,8 +40,6 @@ public class StompHandler implements ChannelInterceptor {
 
 
 
-
-
         // 헤더 토큰 얻기
         String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
         authorizationHeader = authorizationHeader.substring(1, authorizationHeader.length()-1);
@@ -56,9 +54,6 @@ public class StompHandler implements ChannelInterceptor {
         if(jwtService.validateToken(token)==false){
             throw new RuntimeException("토큰 검증 실패");
         }
-
-
-
 
 
         //get nickname which is stored at member table by using token
