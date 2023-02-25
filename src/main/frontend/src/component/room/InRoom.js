a0cc3a2dacc5d370d618660a6b74e4626b682c5d
“ssohye” <ssohye@icloud.com>
2023-02-26 02:54:46 +0900

    api/ 통해서 프록시 이용으로 고침

--- src/main/frontend/src/component/room/InRoom.js
+++ src/main/frontend/src/component/room/InRoom.js
@@ -19,7 +19,7 @@ function InRoom(){
     const reconnect = useRef(0);
 
     const findRoom = async () => {
-        const response = await axios.get(`http://localhost:8080/chat/room/${roomId}`);
+        const response = await axios.get(`api/chat/room/${roomId}`);
         setRoom(response.data);
     };
 
@@ -33,7 +33,7 @@ function InRoom(){
     };
 
     useEffect(() => {
-        sock.current = new SockJS('http://localhost:8080/ws/chat');
+        sock.current = new SockJS('api/ws/chat');
         ws.current = Stomp.over(sock.current);
         findRoom();
 
@@ -47,7 +47,7 @@ function InRoom(){
             if (reconnect.current++ <= 5) {
                 setTimeout(() => {
                     console.log('connection reconnect');
-                    sock.current = new SockJS('/ws/chat');
+                    sock.current = new SockJS('api/ws/chat');
                     ws.current = Stomp.over(sock.current);
                     ws.current.connect(headers);
                 }, 10 * 1000);
