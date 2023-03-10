fd71832ae3ad05f8563fcad130f6d5724389a107
kimuijin <whitekuj02@naver.com>
2023-03-10 21:43:37 +0900

    react-native 환경 설정 안드로이드 SDK 설정 후 환경 변수 추가 그 후 안드로이드 스튜디오에서 애뮬레이션 초기화 후 사용 메인 창 js로 변경

--- src/main/Appfrontend/index.js
+++ src/main/Appfrontend/index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import Main from './Main';
 import {name as appName} from './app.json';
+import App from './App';
 
 AppRegistry.registerComponent(appName, () => Main);
