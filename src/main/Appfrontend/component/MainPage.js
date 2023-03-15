1c7693482cf2d93a2ccf74077f28cdeb3c2f03b5
kimuijin <whitekuj02@naver.com>
2023-03-15 11:50:20 +0900

    mainpage

--- src/main/Appfrontend/component/MainPage.js
+++ src/main/Appfrontend/component/MainPage.js
@@ -5,6 +5,7 @@ import {
     StatusBar,
     StyleSheet,
     Text,
+    TouchableOpacity,
     useColorScheme,
     Image,
     View,
@@ -19,37 +20,76 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import MainLog from '../img/logo.png';
-
-const styles = StyleSheet.create({
-    button: {
-        background: "#E87D30",
-        borderRadius: "40",
-    },
-    logo: {
-        width: "60",
-        height: "60",
-    }
-});
 function MainPage(props){
-
     return(
-        <View>
-            <Text> SSopa </Text>
-            <Image source = {require('../img/logo.png')}
-                   style={styles.logo}/>
-            <Button title="Login"
-                    style = {styles.button}
-                    onPress={()=>{
-                        props.navigation.navigate('Login')
-                    }}/>
-            <Button title="SignUp"
-                    style = {styles.button}
-                    onPress={()=>{
-                        props.navigation.navigate('SignUp')
-                    }}/>
+        <View style={styles.container}>
+            <View style={{flex: 2}}></View>
+            <Image source={require('../img/logo.png')} style={styles.logo}/>
+            <Text style={styles.textMain}>SSOPA와 함께하는 학교 생활</Text>
+            <View style={{flex: 2}}></View>
+            <View style={{flexDirection: 'row', flex: 2}}>
+                <TouchableOpacity style={styles.buttonLogin} onPress={()=>{
+                    props.navigation.navigate('Login')
+                }}>
+                    <Text style={styles.textLogin}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonSignUp} onPress={()=>{
+                    props.navigation.navigate('SignUp')}}>
+                    <Text style={styles.textSignUp}>SignUp</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    buttonLogin: {
+        backgroundColor: "#E87D30",
+        borderRadius: 6,
+        width: "40%",
+        height: "35%",
+        marginLeft: 10,
+        justifyContent: 'center',
+    },
+    buttonSignUp: {
+        backgroundColor: "white",
+        borderRadius: 6,
+        width: "40%",
+        height: "35%",
+        marginLeft: 10,
+        borderStyle: "solid",
+        borderWidth: 0.8,
+        borderColor: "#888",
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 200,
+        height: 140,
+        marginBottom: 20,
+    },
+    textMain: {
+        textAlign: 'center',
+        color: "#E87D30",
+        fontWeight: "bold",
+        fontSize: 25,
+    },
+    textLogin: {
+        textAlign: 'center',
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 25,
+    },
+    textSignUp: {
+        textAlign: 'center',
+        fontWeight: "bold",
+        color: "#444",
+        fontSize: 25,
+    }
+});
 
 export {MainPage}
