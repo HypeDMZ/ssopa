a99d490048a723ade835a82ff3c52c9717a05d6b
“ssohye” <ssohye@icloud.com>
2023-03-30 14:52:16 +0900

    임시테스트용으로 토큰 만료시간 수정

--- src/main/java/com/example/demo/jwt/TokenProvider.java
+++ src/main/java/com/example/demo/jwt/TokenProvider.java
@@ -29,9 +29,9 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; //30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1; //30분
 
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; //24시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 2; //24시간
     private final Key key;
 
     private final Key key4refresh;
