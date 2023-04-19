99fe561b46380a7c0b2a44c61b62dad87e04f9b6
“ssohye” <ssohye@icloud.com>
2023-04-19 22:19:09 +0900

    좋아요 버튼 누를시 푸시 알림 작동하도록 변경

---
+++ src/main/java/com/example/demo/Service/ApnsPushService.java
@@ -0,0 +1,115 @@
+package com.example.demo.Service;
+
+import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.ApnsClientBuilder;
+import com.eatthepath.pushy.apns.ApnsPushNotification;
+import com.eatthepath.pushy.apns.auth.ApnsSigningKey;
+import com.eatthepath.pushy.apns.util.ApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
+import com.eatthepath.pushy.apns.util.TokenUtil;
+import com.example.demo.entity.DeviceToken;
+import com.example.demo.entity.Member;
+import com.example.demo.entity.PushPayload;
+import com.example.demo.repository.DeviceTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+
+@Service
+@RequiredArgsConstructor
+public class ApnsPushService {
+    private static ApnsClient apnsClient;
+
+    private static DeviceTokenRepository deviceTokenRepository;
+
+    void init() {
+        try {
+            String teamId = "CFQ4ZSUWR7";
+            String keyId = "2QPURU9C8F";
+            apnsClient = new ApnsClientBuilder()
+                    .setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST)
+                    .setSigningKey(ApnsSigningKey.loadFromPkcs8File(new File("src/main/resources/apnsPushKey.p8"),
+                            teamId, keyId))
+                    .build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SimpleApnsPushNotification getPushNotification(PushPayload content, String deviceToken){
+        final SimpleApnsPushNotification pushNotification;
+
+        {
+            final ApnsPayloadBuilder payloadBuilder = new SimpleApnsPayloadBuilder();
+            payloadBuilder.setAlertTitle(content.getAlertTitle());
+            payloadBuilder.setAlertBody(content.getAlertBody());
+            payloadBuilder.setSoundFileName(content.getSound());
+
+            final String payload = payloadBuilder.build();
+            final String token = TokenUtil.sanitizeTokenString(deviceToken);
+
+            pushNotification = new SimpleApnsPushNotification(token, "dmz.ssopa", payload);
+
+            return pushNotification;
+        }
+    }
+
+    public void sendPushByMember(List<Member> members, PushPayload payload){
+        ArrayList<String> deviceTokens = new ArrayList<>();
+
+        members.forEach(member -> {
+            List<DeviceToken> tokens = deviceTokenRepository.findAllByMemberId(member);
+            tokens.forEach(token -> {
+                deviceTokens.add(token.getToken());
+            });
+        });
+        sendPush(deviceTokens, payload);
+    }
+
+    public void sendPush(List<String> deviceTokens, PushPayload payload) {
+
+        List<ApnsPushNotification> collectionOfPushNotifications = new ArrayList<>();
+        deviceTokens.forEach(deviceToken -> {
+            SimpleApnsPushNotification pushNotification = getPushNotification(payload, deviceToken);
+            collectionOfPushNotifications.add(pushNotification);
+        });
+
+
+        for (final ApnsPushNotification pushNotification : collectionOfPushNotifications) {
+            final CompletableFuture sendNotificationFuture = apnsClient.sendNotification(pushNotification);
+
+            sendNotificationFuture.whenComplete((response, cause) -> {
+                // This will get called when the sever has replied. response will
+                // be null and cause will be non-null if something went wrong when
+                // sending the notification.
+
+                if (response != null) {
+                    // Handle the push notification response as before from here.
+                } else {
+                    // Something went wrong when trying to send the notification to the
+                    // APNs server. Note that this is distinct from a rejection from
+                    // the server, and indicates that something went wrong when actually
+                    // sending the notification or waiting for a reply.
+                    System.out.print("Failed to send push notification :");
+                    System.out.println(cause);
+                }
+            });
+        }
+    }
+}
+
+
