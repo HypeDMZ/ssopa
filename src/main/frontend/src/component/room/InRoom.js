a050cf535d5833885ecdd48658100e16a7b879b8
“ssohye” <ssohye@icloud.com>
2023-04-21 09:18:35 +0900

    수정

--- src/main/frontend/src/component/room/InRoom.js
+++ src/main/frontend/src/component/room/InRoom.js
@@ -24,7 +24,7 @@ function InRoom(props){
     const reconnect = useRef(0);
 
     const client = new StompJs.Client({
-        brokerURL: 'wss://ssopa02.com/api/ws/chat',
+        brokerURL: 'wss://ssopa02.com/api/ws/chat/websocket',
         connectHeaders: headers,
         debug: function (str) {
             console.log(str);
